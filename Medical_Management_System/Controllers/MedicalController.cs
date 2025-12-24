5cd8778349afcba41302296ba33d4258eb3d1bf0
Samadhan Patil <samadhan0018@gmail.com>
2025-12-24 22:57:00 +0530

    Medical Controller update

---
+++ Medical_Management_System/Controllers/MedicalController.cs
@@ -0,0 +1,29 @@
+﻿using Medical_Management_System.Data;
+using Medical_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medical_Management_System.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicalController : ControllerBase
+    {
+        private readonly MedicalDbContext _context;
+
+        // Constructor (Dependency Injection)
+        public MedicalController(MedicalDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/medical
+        [HttpPost]
+        public IActionResult CreateMedical(Medical medical)
+        {
+            _context.Medical.Add(medical);
+            _context.SaveChanges();
+
+            return Ok(medical);
+        }
+    }
+}
